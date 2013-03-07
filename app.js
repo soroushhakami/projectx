9cd62238b1541d9f2931b33af207499070bae22b
Soroush Hakami <soroush.hakami@gmail.com>
2013-03-07 13:59:58 +0100

    changed template engine to handlebars

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var app = express();
 app.configure(function(){
   app.set('port', process.env.PORT || port);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'hbs');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());

1456e79f6e36a1ba9e7637052cd5380960bab752
Soroush Hakami <soroush.hakami@gmail.com>
2013-03-26 08:13:08 +0100

    removed unused functions

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
 
-/**
- * Module dependencies.
- */
-
 var express = require('express'),
   routes = require('./routes'),
   http = require('http'),
@@ -34,25 +30,6 @@ var users = [{
   password: 'birthday',
   email: 'joe@example.com'
 }];
-
-function findById(id, fn) {
-  var idx = id - 1;
-  if (users[idx]) {
-    fn(null, users[idx]);
-  } else {
-    fn(new Error('User ' + id + ' does not exist'));
-  }
-}
-
-function findByUsername(username, fn) {
-  for (var i = 0, len = users.length; i < len; i++) {
-    var user = users[i];
-    if (user.username === username) {
-      return fn(null, user);
-    }
-  }
-  return fn(null, null);
-}
 
 
 // Passport session setup.

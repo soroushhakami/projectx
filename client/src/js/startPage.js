1776efdc0030b1655b55bddb713c890f7564e7b8
Soroush Hakami <soroush.hakami@gmail.com>
2013-03-27 18:43:45 +0100

    removed unused functions

--- client/src/js/startPage.js
+++ client/src/js/startPage.js
@@ -1,7 +1,7 @@
 var StartPage = (function () {
 
     var render = function () {
-        //let's fire it up
+        //let's fire it up!
         $(document).attr('title', 'Pairmodoro');
         var startHtml = JST['views/clientside/start.hbs']();
         $('#main_display').empty().append(startHtml);

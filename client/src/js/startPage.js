2f45f02af88ce4cdad6caef525f2b0691e78e08e
Soroush Hakami <soroush.hakami@gmail.com>
2013-03-27 19:16:25 +0100

    reformat

--- client/src/js/startPage.js
+++ client/src/js/startPage.js
@@ -7,8 +7,8 @@ var StartPage = (function () {
         $('#main_display').empty().append(startHtml);
         Pair.emptyPair();
 
-        $.getJSON('/users', function(users){
-            $(users).each(function(i, user){
+        $.getJSON('/users', function (users) {
+            $(users).each(function (i, user) {
                 var userTemplate = JST['views/clientside/user.hbs'](user);
                 $('#users').append(userTemplate);
                 Users.addUser(user);
@@ -18,47 +18,47 @@ var StartPage = (function () {
         });
     };
 
-    var initEventHandlers = function(){
-      $('.user').click(function(){
-          var userAvatarElement = $(this).find('.avatar');
-          var userNameText = $(this).find('.name').text();
+    var initEventHandlers = function () {
+        $('.user').click(function () {
+            var userAvatarElement = $(this).find('.avatar');
+            var userNameText = $(this).find('.name').text();
 
-          if (!Pair.isFullPair()){
-              userAvatarElement.toggleClass('selected');
+            if (!Pair.isFullPair()) {
+                userAvatarElement.toggleClass('selected');
 
-              if(userAvatarElement.hasClass('selected')){
-                  Pair.addUser(userNameText);
-              } else{
-                  Pair.removeUser(userNameText);
-              }
-          } else if (Pair.hasUser(userNameText)){
-              userAvatarElement.toggleClass('selected');
-              Pair.removeUser(userNameText);
-          }
-      });
+                if (userAvatarElement.hasClass('selected')) {
+                    Pair.addUser(userNameText);
+                } else {
+                    Pair.removeUser(userNameText);
+                }
+            } else if (Pair.hasUser(userNameText)) {
+                userAvatarElement.toggleClass('selected');
+                Pair.removeUser(userNameText);
+            }
+        });
 
-      $('.chart-icon').click(function(){
-          var userNameText = $(this).parent().find('.name').text();
-          /*$(this).tooltip('destroy');*/
-          StatsPage.render(userNameText);
-      });
+        $('.chart-icon').click(function () {
+            var userNameText = $(this).parent().find('.name').text();
+            /*$(this).tooltip('destroy');*/
+            StatsPage.render(userNameText);
+        });
 
         /*$('.chart-icon').tooltip({
-            'placement': 'left',
-            'trigger': 'hover',
-            'title': function(){
-                return 'View stats for ' + $(this).parent().find('.name').text();
-            }
-        });*/
+         'placement': 'left',
+         'trigger': 'hover',
+         'title': function(){
+         return 'View stats for ' + $(this).parent().find('.name').text();
+         }
+         });*/
 
-      $('#startBtn').click(function(){
-          Notification.askForPermissions();
-          TimerPage.render();
-      });
+        $('#startBtn').click(function () {
+            Notification.askForPermissions();
+            TimerPage.render();
+        });
     };
 
     return {
-        render:render
+        render: render
     };
 })();
 
